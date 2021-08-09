dcd7e1bb49b4b38a9b4dc326f09fc50e413dacab
Sungho Lee <sh1217sh@gmail.com>
2021-08-09 20:57:16 +0900

    Initial commit of Arduino code for weather station

---
+++ weather_station/station/secrets.h
@@ -0,0 +1,7 @@
+// IMPORTANT: DO NOT ADD THIS FILE TO GIT.
+// This file is intentinally left out from version control.
+// Always check if this file is ignored by git.
+
+#define SECRET_WIFI_SSID "<YOUR_SSID>"
+#define SECRET_WIFI_PW "<YOUR_PW>"
+#define SECRET_MQTT_HOST "<YOUR_BROKER_IP>"
